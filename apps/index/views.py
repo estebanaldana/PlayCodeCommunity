7cf1bb9c5810e31bf562064b0dd7cf6bd3c29683
estebanaldana <dantes-_inferno@hotmail.com>
2018-05-29 13:14:06 -0500

    Start Index View

---
+++ apps/index/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from apps.proyects.models import Products
+from apps.community.models import Community
+from django.core.exceptions import ObjectDoesNotExist
+from django.views.defaults import page_not_found, server_error
+
+
+
+def index(request):
+
+	try:
+		queryset_list_proyect = Products.objects.filter(id__isnull=False).latest('id')
+	except ObjectDoesNotExist:
+		queryset_list_proyect = None
+
+	try:	
+		queryset_list_community = Community.objects.filter(id__isnull=False).latest('id')
+	except ObjectDoesNotExist:
+		queryset_list_community = None
+
+
+	context={
+ 		"object_list_proyect" : queryset_list_proyect,
+ 		"object_list_community" : queryset_list_community,
+	}
+	return render(request, 'base/index.html', context)
+
+def error_404(request):
+	return render(request, '404.html', status=404)
+
+def error_500(request):
+	return render(request, '500.html', status=500)
