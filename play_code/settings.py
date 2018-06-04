df652fce708942360d40db56b23859f1149185b5
estebanaldana <dantes-_inferno@hotmail.com>
2018-06-03 22:48:45 -0500

    syntax error corrections

--- play_code/settings.py
+++ play_code/settings.py
@@ -15,14 +15,14 @@ from django.core.urlresolvers import reverse_lazy
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)) 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
