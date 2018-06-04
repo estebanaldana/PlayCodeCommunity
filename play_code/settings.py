3dc3640d2b8089156ba719a0392c8b00826ffa49
estebanaldana <dantes-_inferno@hotmail.com>
2018-06-04 08:42:34 -0500

    Remove whitenoise

--- play_code/settings.py
+++ play_code/settings.py
@@ -145,7 +145,7 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 #MEDIA_URL =  '/media/'
